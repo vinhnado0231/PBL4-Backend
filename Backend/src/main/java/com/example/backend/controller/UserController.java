411547bc7a0f84e98359b7aea5a992d817ea9cee
vinhnado0231 <vinh.nd0231@gmail.com>
2022-12-26 15:12:33 +0700

    ok

--- Backend/src/main/java/com/example/backend/controller/UserController.java
+++ Backend/src/main/java/com/example/backend/controller/UserController.java
@@ -2,17 +2,21 @@ package com.example.backend.controller;
 
 import com.example.backend.dto.UserCreateDTO;
 import com.example.backend.dto.UserDTO;
+import com.example.backend.model.User;
 import com.example.backend.service.impl.AccountService;
+import com.example.backend.service.impl.FriendService;
 import com.example.backend.service.impl.UserService;
-import com.example.backend.ultil.FriendRequest.FriendRecommend1;
-import com.example.backend.ultil.FriendRequest.FriendRecommend2;
-import com.example.backend.ultil.ScheduledTasks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -22,6 +26,8 @@ public class UserController {
     private UserService userService;
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private FriendService friendService;
 
     @GetMapping("/check-valid-username")
     public ResponseEntity<Object> checkUsername(@RequestParam String username) {
@@ -51,6 +57,23 @@ public class UserController {
 //        LinkedList<UserDTO> list = FriendRecommend2.ListRecommend(1);
 //        return new ResponseEntity<>(list,HttpStatus.OK);
 //    }
+
+    @GetMapping("/get-info-user")
+    public ResponseEntity<Object> checkEmail(@RequestParam long idUser, Authentication authentication) {
+        try{
+            Map<String, Object> map = new HashMap<String, Object>();
+            User user = userService.getUserByIdUser(idUser);
+            user.setAccount(null);
+            user.setEmailUser(null);
+            user.setPhoneUser(null);
+            map.put("user",user);
+            long mutualFriend = friendService.getMutualFriend(idUser,accountService.getIdUserByUsername(authentication.getName()));
+            map.put("mutualFriend",mutualFriend);
+            return new ResponseEntity<>(map,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
     @PostMapping("/create-new-user")
     public ResponseEntity<Object> createUser(@RequestBody UserCreateDTO user) {
